35648b2aa20c775637c9e0246ad224210f266c71
“Kunal <kunalkoushik44@gmail.com>
2024-08-14 22:05:47 +0530

    Navbar

--- Client/src/component/Header.jsx
+++ Client/src/component/Header.jsx
@@ -6,10 +6,12 @@ const Header = () => {
   return (
     <nav className="bg-slate-200 shadow-md">
       <div className=" flex justify-between items-center max-w-6xl mx-auto p-2">
+        <Link to='/'>
         <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
-          <span className="text-slate-500">Real</span>
-          <span className="text-slate-700">Estate</span>
+          <span className="text-slate-700">Heaven</span>
+          {/* <span className="text-slate-700">Estate</span> */}
         </h1>
+        </Link>
         <form className="bg-slate-100 p-2 flex items-center  rounded-lg">
           <input
             type="text"
@@ -20,10 +22,16 @@ const Header = () => {
         </form>
 
         <ul className="flex gap-4">
+          <Link to='/'>
             <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
+          </Link>
+          <Link to='/about'>
             <li  className="hidden sm:inline text-slate-700 hover:underline" >About</li>
-            <li  className="hidden sm:inline text-slate-700 hover:underline" >Sign in</li>
-            {/* <li >Sign up</li> */}
+          </Link>
+          <Link to='/sign-in'>
+            <li  className=" sm:inline text-slate-700 hover:underline" >Sign in</li>
+          </Link>
+        
         </ul>
       </div>
     </nav>
